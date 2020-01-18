a519530792e1ce888a2095359370038cc9a90ca9
Taylor von Hausen <tvonhaus@uoguelph.ca>
2020-01-18 16:08:32 -0500

    fixed range for x,y coordinates of attacker spawn

--- lights.py
+++ lights.py
@@ -24,9 +24,6 @@ spawn_attacker_event = pyg.USEREVENT + 1
 
 pyg.time.set_timer(spawn_attacker_event, attack_timer)
 
-print(attacker1.rect)
-
-
 game_over = False
 while not game_over:
     for event in pyg.event.get():
@@ -39,23 +36,23 @@ while not game_over:
             attacker_y = 0
             wall = random.randint(0,3)
             if wall == 0:
-                new_x = random.randint(100,700)
+                new_x = random.randint(50,750)
                 new_y = 0
                 attacker_x = 50
                 attacker_y = 10
             elif wall == 1:
                 new_x = 790
-                new_y = random.randint(100,500)
+                new_y = random.randint(50,550)
                 attacker_x = 10
                 attacker_y = 50
             elif wall == 2:
-                new_x = random.randint(100,700)
+                new_x = random.randint(50,750)
                 new_y = 590
                 attacker_x = 50
                 attacker_y = 10
             elif wall == 3:
                 new_x = 0
-                new_y = random.randint(100,500)
+                new_y = random.randint(50,550)
                 attacker_x = 10
                 attacker_y = 50
             attacker1.rect = (new_x,new_y,attacker_x,attacker_y)
