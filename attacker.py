8f2311e5f49fcaef1bfb74d675b5f54078f5c44f
Taylor von Hausen <tvonhaus@uoguelph.ca>
2020-01-19 15:28:13 -0500

    Added spawn_attacker(), flash() methods to attacker class

--- attacker.py
+++ attacker.py
@@ -1,9 +1,46 @@
 import pygame as pyg
+import random
+import time
 
 class Attacker(object):
     def __init__(self):
         self.rect = pyg.Rect((500,0,50,10))
 
-    def draw(self, display):
-            pyg.draw.rect(display,(255,0,0),self.rect)
+    def draw(self, color, display):
+            pyg.draw.rect(display, color, self.rect)
 
+    def spawn_attacker(self):
+            new_x = 0
+            new_y = 0
+            attacker_x = 0
+            attacker_y = 0
+            wall = random.randint(0,3)
+            if wall == 0:
+                new_x = random.randint(50,750)
+                new_y = 0
+                attacker_x = 50
+                attacker_y = 10
+            elif wall == 1:
+                new_x = 790
+                new_y = random.randint(50,550)
+                attacker_x = 10
+                attacker_y = 50
+            elif wall == 2:
+                new_x = random.randint(50,750)
+                new_y = 590
+                attacker_x = 50
+                attacker_y = 10
+            elif wall == 3:
+                new_x = 0
+                new_y = random.randint(50,550)
+                attacker_x = 10
+                attacker_y = 50
+        
+            return new_x, new_y, attacker_x, attacker_y
+
+    def flash(self,display):
+        pyg.display.update()
+        self.draw((255,255,255), display)
+        pyg.display.update()
+        self.draw((255,0,0), display)
+        pyg.display.update()
--- lights.py
+++ lights.py
@@ -13,13 +13,14 @@ pyg.display.set_caption('Lights')
 
 white = (255,255,255)
 black = (0,0,0)
-attack_timer = 500
+attack_timer = 1000
 
 
 player = Player()
 attacker1 = Attacker()
 clock = pyg.time.Clock()
 
+print(attacker1.rect)
 spawn_attacker_event = pyg.USEREVENT + 1
 
 pyg.time.set_timer(spawn_attacker_event, attack_timer)
@@ -30,39 +31,16 @@ while not game_over:
         if event.type==pyg.QUIT:
             game_over = True
         elif event.type == spawn_attacker_event:
-            new_x = 0
-            new_y = 0
-            attacker_x = 0
-            attacker_y = 0
-            wall = random.randint(0,3)
-            if wall == 0:
-                new_x = random.randint(50,750)
-                new_y = 0
-                attacker_x = 50
-                attacker_y = 10
-            elif wall == 1:
-                new_x = 790
-                new_y = random.randint(50,550)
-                attacker_x = 10
-                attacker_y = 50
-            elif wall == 2:
-                new_x = random.randint(50,750)
-                new_y = 590
-                attacker_x = 50
-                attacker_y = 10
-            elif wall == 3:
-                new_x = 0
-                new_y = random.randint(50,550)
-                attacker_x = 10
-                attacker_y = 50
+            new_x , new_y, attacker_x, attacker_y = attacker1.spawn_attacker() 
             attacker1.rect = (new_x,new_y,attacker_x,attacker_y)
-
+            #attacker1.flash(dis)
 
     dis.fill(black)
     player.draw(dis)
     player.player_control()
     player.update(dis)
-    attacker1.draw(dis)
+    attacker1.draw((255,0,0), dis)
+    attacker1.flash(dis)
     pyg.display.update()
 
     clock.tick(30)
